0b10901c8feb780236089a9a76c51fdece96db5d
suqiang <suqiang@jf.com>
2018-05-27 23:12:22 +0800

    第一次提交

---
+++ app/src/main/java/bar/mytest/com/gittest/MainActivity.java
@@ -0,0 +1,21 @@
+package bar.mytest.com.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        Log.d(TAG,"测试");
+    }
+}
